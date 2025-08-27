3f59b6a3fdc42d3991c14d2814bdea91b4e41bd8
JGaldo-beep <juan.galdo@ucb.edu.bo>
2025-08-26 22:23:32 -0400

    feat(form): Adding SearchForm feature

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import SearchForm from "../components/SearchForm";
 
-const Home = () => {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { query: string };
+}) {
+  const query = (await searchParams).query;
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading">
+          Join our community of innovative thinkers and take your startup to the
+          next level.
+        </p>
+        <SearchForm query={query} />
+      </section>
     </>
   );
-};
-
-export default Home;
+}
